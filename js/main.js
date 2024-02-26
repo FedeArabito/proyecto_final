d8ca820b013a2d708c01efd4b4c3afb2a23d31d5
Federico Arabito <federicoarabito1999@gmail.com>
2024-02-26 18:34:37 -0300

    Pre Entrega 2

--- js/main.js
+++ js/main.js
@@ -1,32 +1,4 @@
-//PRE ENTREGA 1
-/* const validacionUS = () => {
-  let usuario = "Fede99";
-  let contraseña = "12345";
-  alert("Bienvenido a Sneaker ID, porfavor introduzca su usuario y contraseña");
-  let ingresoUsuario = prompt("Usuario: ");
-  let ingresoContraseña = prompt("Contraseña: ");
-  if (ingresoUsuario !== usuario && ingresoContraseña!==contraseña) {
-      for (let i = 3; i > 0; i--){
-      alert("Usuario o contraseña incorrecto, te quedan "+ i+ " intentos, vuelve a intentarlo");
-      let ingresoUs2 = prompt("Usuario: ");
-      let ingresoCon2 = prompt("Contraseña: ");
-      if (ingresoUs2== usuario && ingresoCon2==contraseña) {
-        alert("Hola " +usuario+ " ,bienvenido a la tienda oficial de Sneaker ID.");
-        return false;
-      }
-  }
-  alert("Te has quedado sin intentos, vuelve a intentarlo mas tarde...")
-    
-  return false;
-}else if(ingresoUsuario == usuario && ingresoContraseña==contraseña){
-  alert("Hola " +usuario+ " ,bienvenido a la tienda oficial de Sneaker ID.");
-}
-
-}
-
-
-validacionUS();  */
-
+//PRE ENTREGA 2
 
 
 const sneakers = [
@@ -48,36 +20,43 @@ const sneakers = [
   }
 ];
 
-
-let seleccionZapa = prompt("Que desea comprar?");
-// const validarInput = (input, arr) => {
-//   input;
-//   for (const el of arr) {
-//     const nombre = arr.nombre;
-//     const precio = arr.precio;
-//     if (input == nombre) {
-//       return prompt("Genial, " + input + " ha sido agregado a su carrito, el total a pagar es de " + precio)
-//     }
-//     else{
-//       return alert("Lo sentimos no contamos con ese producto")
-//     }
-//   }
-// }
-// validarInput(seleccionZapa, sneakers);
-
-function validarCompra(input, arrayProductos) {
-  // Buscar el producto en el array
-  const productoEncontrado = arrayProductos.find(
+const validarCompra = (input, arr) => {
+  const productoEncontrado = arr.find(
     (producto) => producto.nombre === input
   );
-  // Si el producto se encuentra
   if (productoEncontrado) {
-    // Mostrar mensaje de compra
-    alert(`Muchas gracias por comprar ${productoEncontrado.nombre}!`);
+    alert(`Muchas gracias por comprar ${productoEncontrado.nombre}! el precio es de ${productoEncontrado.precio}`);
   } else {
-    // Mostrar mensaje de error
     alert("El producto no se encuentra en la lista");
   }
-}
+};
 
-validarCompra(seleccionZapa,sneakers);
+const validacionUS = () => {
+  let usuario = "Fede99";
+  let contraseña = "12345";
+  alert("Bienvenido a Sneaker ID, por favor introduzca su usuario y contraseña");
+  
+  for (let i = 3; i > 0; i--) {
+    let ingresoUsuario = prompt("Usuario: ");
+    let ingresoContraseña = prompt("Contraseña: ");
+    
+    if (ingresoUsuario === usuario && ingresoContraseña === contraseña) {
+      alert("Hola " + usuario + " ,bienvenido a la tienda oficial de Sneaker ID.");
+      let seleccionZapa = prompt("¿Qué desea comprar?");
+      validarCompra(seleccionZapa, sneakers);
+      return;
+    } else {
+      alert("Usuario o contraseña incorrecto, te quedan " + i + " intentos, vuelve a intentarlo");
+    }
+  }
+  
+  alert("Te has quedado sin intentos, vuelve a intentarlo más tarde...");
+};
+
+validacionUS();
+
+
+
+
+
+
